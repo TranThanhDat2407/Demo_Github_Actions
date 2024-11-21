1224a3c828c1c51bc3e061ac6a454b2f26e0b944
TranThanhDat2407 <tranthanhdat2407.email@gmail.com>
2024-11-21 19:21:53 +0700

    first commit

---
+++ src/main/java/org/example/Calculator.java
@@ -0,0 +1,13 @@
+package org.example;
+
+public class Calculator implements ICalculator{
+    @Override
+    public Integer sum(Integer a, Integer b) {
+        return 0;
+    }
+
+    @Override
+    public Integer sub(Integer a, Integer b) {
+        return 0;
+    }
+}
